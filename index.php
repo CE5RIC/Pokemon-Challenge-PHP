b2bea00c8d4e167fad263298415d685f47a7d2d6
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-14 14:51:48 +0200

    index files from pokedex added

--- index.php
+++ index.php
@@ -4,9 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokedex build with PHP</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>AJAX Pokédex</title>
 </head>
 <body>
+<div>
+   <img class="background" src="Images/amended pokemon.jpeg" alt="151 original pokemon">
+</div>
+
+
+<section>
+    <div class="content">
+        <div class="content-header">
+          <div class="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png" alt="" />
+          </div>
+          <div class="searchPokemon">
+            <input id="pokemon" type="text" placeholder="Search Pokemon" />
+            <button id="search" class="button-search">
+              <div class="button-content">
+                Gotta catch them all!
+                <img src="http://www.purarteadesivos.com.br/wp-content/uploads/2017/04/Pok%C3%A9mon-go.png" alt="" />
+              </div>
+            </button>
+          </div>
+        </div>
+
+</section>
+<div class="display">
+<div class="displayPokemon">
+    <img id="image" src="" alt="">
+    </div>
     
+    <div class="pokemoninfo">
+        <h1></h1>
+    <h2 id="pokemonId"></h2>
+    <p id="photo"></p>
+    <li id="move" class="moves"></li>
+    <li id="move2" class="moves"></li>
+    <li id="move3" class="moves"></li>
+    <li id="move4" class="moves"></li>
+    </div>
+</div>
+   
+   
+    <script src="script.js"></script>
 </body>
 </html>
