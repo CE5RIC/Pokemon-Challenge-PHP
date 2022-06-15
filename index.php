63c63aa4c890fb2fc8734215877d19fe0dcdc472
CE5RIC <cedricdierckx5@hotmail.com>
2022-06-15 14:47:15 +0200

    Option to find pokemon on name and number

--- index.php
+++ index.php
@@ -1,22 +1,10 @@
-<?php
 
-echo "Hello World";
-
-
-//POKEMON API URL
-$api_url = 'https://pokeapi.co/api/v2/pokemon/1';
-
-
-//READ THE JSON FILE
-$json_data = file_get_contents($api_url);
-
-?>
-
-<a href="pokemon-challenge-php">
+<a href="pokemon-challenge-php"></a>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,49 +13,33 @@ $json_data = file_get_contents($api_url);
 </head>
 <body>
 
+<form class="input-text" action="" method="GET">
+                Name: <input class="input" id="userInput" type="text" name="name" value="">
+                <br>
+                <input type="submit" id="btnNumbInput" value="">
+            </form>
+    <?php
+           
+
+            $api_url = "https://pokeapi.co/api/v2/pokemon/" . $_GET['name'];
+            $json = file_get_contents($api_url);
+
+            $data_res = json_decode($json, true);
+            $name = $data_res['forms']['0']['name'];
+           // echo $id;echo "<br>";
+            $id = $data_res['id'];
+            $image = $data_res['sprites']['back_default'];
+    ?>
+
+            <div class="container">
+                Id of the pokemon: <h4><?php echo $id;?></h4>
+            </div>
+
+<img src="<?php echo $image;?>">
 
 
-<div>
-   <img class="background" src="Images/amended pokemon.jpeg" alt="151 original pokemon">
-</div>
 
 
-<section>
-    <div class="content">
-        <div class="content-header">
-          <div class="logo">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png" alt="" />
-          </div>
-          <div class="searchPokemon">
-            <input id="pokemon" type="text" placeholder="Search Pokemon" />
-            <button id="search" class="button-search">
-              <div class="button-content">
-                Gotta catch them all!
-                <img src="http://www.purarteadesivos.com.br/wp-content/uploads/2017/04/Pok%C3%A9mon-go.png" alt="" />
-              </div>
-            </button>
-          </div>
-        </div>
-
-</section>
-<div class="display">
-<div class="displayPokemon">
-    <img id="image" src="" alt="">
-    </div>
-    
-    <div class="pokemoninfo">
-        <h1></h1>
-    <h2 id="pokemonId"></h2>
-    <p id="photo"></p>
-    <li id="move" class="moves"></li>
-    <li id="move2" class="moves"></li>
-    <li id="move3" class="moves"></li>
-    <li id="move4" class="moves"></li>
-    </div>
-</div>
-   
-   
-    <script src="script.js"></script>
 </body>
 </html>
 
